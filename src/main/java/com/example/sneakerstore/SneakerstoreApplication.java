8a0bcd0fe596ce5a30e6763e566bec2ecdabcfed
Adwaith <rollo12k@gmail.com>
2026-03-04 22:56:33 +0530

    Add files via upload

---
+++ src/main/java/com/example/sneakerstore/SneakerstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sneakerstore/SneakerstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
